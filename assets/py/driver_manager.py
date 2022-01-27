d1cd5126f952d32560fd1516a1d32477c047031f
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2022-01-27 04:51:40 +0300

    Local Driver
    - Uploaded a local linux webdriver.

--- assets/py/driver_manager.py
+++ assets/py/driver_manager.py
@@ -1,6 +1,4 @@
 import logging
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -9,9 +7,6 @@ from selenium import webdriver
 class DriverManager:
     @staticmethod
     def create_driver():
-        service = Service(ChromeDriverManager(
-            log_level=logging.ERROR).install())
-
         chrome_options = Options()
 
         chrome_options.add_argument("--disable-extensions")
@@ -24,5 +19,5 @@ class DriverManager:
             'excludeSwitches', ['enable-logging'])
 
         driver = webdriver.Chrome(
-            service=service, options=chrome_options)
+            options=chrome_options, executable_path="../webdrivers/chromedriver")
         return driver
