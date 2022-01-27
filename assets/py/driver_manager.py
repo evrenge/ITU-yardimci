c43607b80c26fcfa9af8b396b6e9dbf4b3b8d52f
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2022-01-27 15:50:39 +0300

    Multi OS Drivers
    - Added drivers for mac OS & windows.

--- assets/py/driver_manager.py
+++ assets/py/driver_manager.py
@@ -1,10 +1,25 @@
-import logging
+from lib2to3.pgen2.driver import Driver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from sys import platform
 
 
 class DriverManager:
+
+    DRIVER_WIN_PATH = "../webdrivers/chromedriver_win.exe"
+    DRIVER_MAC_PATH = "../webdrivers/chromedriver_mac"
+    DRIVER_LINUX_PATH = "../webdrivers/chromedriver_linux"
+
+    @staticmethod
+    def get_driver_path():
+        if platform == "linux":
+            return DriverManager.DRIVER_LINUX_PATH
+        elif platform == "darwin":
+            return DriverManager.DRIVER_MAC_PATH
+        elif platform == "win32":
+            return DriverManager.DRIVER_WIN_PATH
+
     @staticmethod
     def create_driver():
         chrome_options = Options()
@@ -19,5 +34,5 @@ class DriverManager:
             'excludeSwitches', ['enable-logging'])
 
         driver = webdriver.Chrome(
-            options=chrome_options, executable_path="../webdrivers/chromedriver")
+            options=chrome_options, executable_path=DriverManager.get_driver_path())
         return driver
