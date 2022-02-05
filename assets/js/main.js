cd0711b1bd77998726d06b5b28eb80faaa591a11
evrenge <evrengeydirici@gmail.com>
2022-02-05 19:57:47 +0300

    UI Tweaks
    - Fixed the #bg backface-visibility issue
    - Changed the fade in/out time to 200ms
    - Created sticky_navbar_content.js for navbar content

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,7 +66,7 @@
 			}
 
 	// Main.
-		var	delay = 325,
+		var	delay = 200,
 			locked = false;
 
 		// Methods.
--- assets/js/prerequisitory.js
+++ assets/js/prerequisitory.js
@@ -29,29 +29,3 @@ function graphPrerequistoryGraph() {
     document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
     document.getElementById("currentStepText").innerHTML = texts[0];
 }
-
-texts = ["Aldığın Dersleri Seç", "Almak İstediğin Dersleri Seç"];
-
-function nextStep() {
-    let currentStepText = document.getElementById("currentStepText");
-    if (prereqGrapher == undefined) {
-        return;
-    } else if (prereqGrapher.graphMode == 0) {
-        prereqGrapher.switchGraphMode(1);
-        document.getElementById("previousStep").disabled = false;
-        currentStepText.innerHTML = texts[1];
-    } else if (prereqGrapher.graphMode == 1) {
-        location.href = "#CoursePlanCreator";
-    }
-}
-
-function previousStep() {
-    let currentStepText = document.getElementById("currentStepText");
-    if (prereqGrapher == undefined) {
-        return;
-    } else if (prereqGrapher.graphMode == 1) {
-        prereqGrapher.switchGraphMode(0);
-        document.getElementById("previousStep").disabled = true;
-        currentStepText.innerHTML = texts[0];
-    }
-}
